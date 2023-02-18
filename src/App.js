0b66e5290007b5948d365dbe0041fe601c36643d
DeepankRx <deepankpushpad10@gmail.com>
2023-02-18 08:14:18 +0530

    new feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import NavBar from './components/NavBar';
-import HomePage from './components/HomePage';
+import NavBar from './components/NavBar/NavBar';
+import HomePage from './components/HomePage/HomePage';
+import Feature from './components/Feature/Feature';
+import Footer from './components/Footer/Footer';
+import WeOffer from './components/We Offer/WeOffer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const App = () => {
   return (
-    <>
+    <Router>
       <NavBar />
-      <HomePage />
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/feature" element={<Feature />} />
+        <Route path="/weoffer" element={<WeOffer />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,21 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div class="flex h-20 items-center  bg-[#293b7a] px-10 text-white border-b-2 border-[#f5986f]">
-      <div class="border-r-4 pr-1 border-[#f5986f] mr-20 text-xl">
-        Marketplace
-      </div>
+      <Link to="/">
+        <div class="border-r-4 pr-1 border-[#f5986f] mr-20 text-xl">
+          Marketplace
+        </div>
+      </Link>
       <div class="hidden lg:flex space-x-4 text-md font-bold flex-1 font-['ubuntu'] ">
-        <div>We Offer</div>
-        <div>Features</div>
-        <div>Subscriptions</div>
-        <div>My orders</div>
-        <div>Resources</div>
+        {/* <div>We Offer</div> */}
+        <Link to="/weoffer">We Offer</Link>
+        {/* <div>Features</div> */}
+        <Link to="/feature">Features</Link>
+        {/* <div>Subscriptions</div> */}
+        {/* <div>My orders</div> */}
+        {/* <div>Resources</div> */}
       </div>
       <div class="hidden lg:flex items-center space-x-4">
         <div class="bg-[#4f6ab4] px-3 py-[6px] rounded-full">Log in</div>
@@ -33,7 +37,7 @@ const NavBar = () => {
             d="M4 6h16M4 12h16M4 18h16"
           />
         </svg>
-    </div>
+      </div>
     </div>
   );
 };
